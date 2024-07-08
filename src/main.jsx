5df0b2f6ad84d69af2c1258ac529f796581076f7
DanielaBadea <badeadaniella@gmail.com>
2024-07-08 18:10:51 +0300

    change main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './Components/App/App'
-import './index.css'
-import { BrowserRouter} from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './Components/App/App';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import  {store, persistor } from './Redux/store';
+import { store, persistor } from './Redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+const rootElement = document.getElementById('root');
+
+// Use ReactDOM.render if you are using React 17 or below
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <PersistGate loading={null} persistor={persistor}>
+//         <BrowserRouter basename='/e-commerce-fragrance'>
+//           <App />
+//         </BrowserRouter>
+//       </PersistGate>
+//     </Provider>
+//   </React.StrictMode>,
+//   rootElement
+// );
+
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-    <BrowserRouter basename='/e-commerce-fragrance' >
-    <App />
-    </BrowserRouter>
-    </PersistGate>
+        <BrowserRouter basename='/e-commerce-fragrance'>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
