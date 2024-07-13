6da7422cf510bb753a764445993082a253a94a2b
DanielaBadea <badeadaniella@gmail.com>
2024-07-13 17:42:45 +0300

    delete icon from head

--- main.jsx
+++ main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-    <BrowserRouter  >
+    <BrowserRouter basename='/e-commerce-fragrance/' >
     <App />
     </BrowserRouter>
     </PersistGate>
