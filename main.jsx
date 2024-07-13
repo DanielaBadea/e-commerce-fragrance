4c3b60d86f9fdae042b717135d6cf0a82410e6fe
DanielaBadea <badeadaniella@gmail.com>
2024-07-13 17:39:15 +0300

    move jsx and css in dist

--- main.jsx
+++ main.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './Components/App/App'
+import App from './src/Components/App/App'
 import './index.css'
 import { BrowserRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
-import  {store, persistor } from './Redux/store';
+import  {store, persistor } from './src/Redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-    <BrowserRouter basename='/e-commerce-fragrance/' >
+    <BrowserRouter  >
     <App />
     </BrowserRouter>
     </PersistGate>
